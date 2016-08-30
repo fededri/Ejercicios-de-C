3edd68eb0e2015da747b30f85433aaa61ce6c882
fededri <fedetto94@hotmail.com>
2016-08-30 00:43:50 -0300

    ejemplo de implementacion de la biblioteca grafic

--- practicas/src/miNivel.c
+++ practicas/src/miNivel.c
@@ -9,22 +9,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <curses.h>
 #include <tad_items.h>
 
 int main(){
 
 	t_list* items = list_create();
 	int rows, cols;
-	int q, p;
+
 
 	nivel_gui_inicializar();
 	nivel_gui_get_area_nivel(&rows, &cols);
 
-	p = cols;
-	q = rows;
+	int xCenter = cols / 2;
+	int yCenter = rows / 2;
 
-	CrearPersonaje(items, '@', p, q);
-	nivel_gui_dibujar(items, "Test Chamber 04");
+	int currentX, currentY;
+	currentX = xCenter;
+	currentY = yCenter;
+
+
+	CrearPersonaje(items, 'S', xCenter++	, yCenter);
+	CrearPersonaje(items, 'a', xCenter++, yCenter);
+	CrearPersonaje(items, 'a', xCenter++, yCenter);
+	CrearCaja(items, 'a', xCenter++, yCenter++,2);
+	CrearCaja(items, 'a', xCenter++, yCenter,2);
+	CrearCaja(items, 'a', xCenter++, yCenter,2);
+	CrearCaja(items, 'a', xCenter++, yCenter,2);
+	CrearCaja(items, 'a', xCenter++, yCenter,2);
+	CrearCaja(items, 'a', xCenter++, yCenter,2);
+	nivel_gui_dibujar(items, "Hola ameo");
+
+
+	while(1){
+		int key = getch();
+
+		switch(key){
+
+		case KEY_UP:
+			currentY--;
+
+			break;
+
+		case KEY_DOWN:
+			currentY++;
+			break;
+
+
+		case KEY_LEFT:
+			currentX--;
+			break;
+
+		case KEY_RIGHT:
+			currentX++;
+
+		break;
+
+
+		case 'q':
+			nivel_gui_terminar();
+			exit(0);
+			break;
+
+		}
+		MoverPersonaje(items,'S', currentX,currentY);
+		nivel_gui_dibujar(items,"Testing");
+
+
+	}
 
 
 
